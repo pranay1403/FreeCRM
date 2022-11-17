849ce01cd482c697733f430e118cce9c6d4efbaa
yogeshgopale <yogeshgopale1812@gmail.com>
2022-11-17 15:20:55 +0530

    yogesh commit

--- src/main/java/com/utility/ReadProp.java
+++ src/main/java/com/utility/ReadProp.java
@@ -9,7 +9,7 @@ public class ReadProp {
 
 	public ReadProp() {
 
-		String path = "E:\\Crm framework\\FreeCRM\\Configfile\\testdata.properties";
+		String path = "E:\\ST\\project\\CRM project\\FreeCRM\\Configfile\\testdata.properties";
 		prop = new Properties();
 
 		try {
